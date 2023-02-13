cddb890cc11ce3bf70953966cc031ff3e7d2e57d
선정관 <sdfjikl8787@gmail.com>
2023-02-13 16:57:59 +0900

    ㅎㅇ

---
+++ 1.core/3.jquery.js
@@ -0,0 +1,7 @@
+$('#myId') // <div id='myId></div>
+
+// 과제: class myClass DOM을 찾아라.
+$('.myClass') // <div class='myClass></div>
+
+// 과제: name attribute value 가  surname 인 input DOM들을 찾아라
+$('input[name=surname]')
